e44100f5eb84b1c83dd877b21ea02fe185cb0b3d
renzhanfei <951255660@qq.com>
2020-08-26 19:02:36 +0800

    * test

---
+++ src/Entities/Repositories/WinningRecordsRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oasis\CustomizeApi\Entities\Repositories;
+
+use Doctrine\ORM\EntityRepository;
+
+class WiningRecordsRepository extends EntityRepository
+{
+}
---
+++ src/Entities/VoteRecords.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Oasis\CustomizeApi\Controllers\Entities;
+
+class VoteRecords
+{
+    
+}
